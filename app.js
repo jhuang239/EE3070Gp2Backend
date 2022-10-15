2cd38f20cb8ae029d3f929e20574b2d0378446ad
KimWong1118 <jk.wong1118@gmail.com>
2022-10-15 16:38:27 +0800

    make it better

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => res.send("Testing"));
+
 //create
 app.post("/insert", (request, response) => {
     const { usr_name, pwd, device_code } = request.body;
